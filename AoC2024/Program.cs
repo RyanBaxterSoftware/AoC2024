3fc6b3765a5372c87370c69241ff0ad19bae1b1d
RyanBaxterSoftware <ryan.baxter.software@gmail.com>
2024-11-30 23:27:51 -0500

    Initial setup of solution.

---
+++ AoC2024/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please choose which day you would like to run?");
+
+var dayEntered = Console.ReadLine();
+while(dayEntered != "Exit") {
+    switch(dayEntered) {
+    case "1":
+        Console.WriteLine("Coming soon!");
+        break;
+    default:
+        Console.WriteLine("I didn't find that day. Here is Hello World instead.");
+        BasicCode.runHelloWorld();
+        break;
+    }
+
+    Console.WriteLine("That was fun! Want to run another? Enter the day or 'Exit'");
+    dayEntered = Console.ReadLine();
+}
